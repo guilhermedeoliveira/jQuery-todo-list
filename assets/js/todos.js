6341c212c61eee4f9e2d3c319c1292d3160201a0
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-07-03 14:28:49 -0300

    Remove li when span delete is clicked

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,11 @@
+// Checked feature
 $('li').on('click', function() {
   $(this).toggleClass('checked');
+});
+
+$('span').on('click', function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
 })
